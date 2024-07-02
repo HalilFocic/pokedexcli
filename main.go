8d1785644d54982ac192eea1c04a53c0bd186392
Halil Focic <halil.focic@zendev.se>
2024-07-02 13:34:49 +0200

    feat: added repl for reading user input and tests for cleanInput function

--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
-    scanner.Scan()
-    text := scanner.Text()
-    fmt.Println(text)
+	startRepl()
 }
+
 
 
